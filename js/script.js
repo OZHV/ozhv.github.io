140529add1870de3bb6e134d866eb0bc9b9e1d32
MrLuit <me@luithollander.nl>
2017-04-10 18:14:37 +0200

    Agenda en archief toegevoegd

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
-var globalhtml = $(".ui.main.text.container").html();
-var firsttime = true;
-$("#search-input").on("input",function() {
-	if($("#search-input").val() != "" && $("#search-input").val().length > 1 && firsttime) {
-		firsttime = false;
-		$(".ui.main.text.container").html($("#results-container").html());
-	}
-	else if($("#search-input").val() != "" && !firsttime) {
-		$(".ui.main.text.container").html($("#results-container").html());
-	}
-	else {
-		$(".ui.main.text.container").html(globalhtml);
-	}
-});
+    var globalhtml = $(".ui.main.text.container").html();
+    var firsttime = true;
+    $("#search-input").on("input", function() {
+        if ($("#search-input").val() != "" && $("#search-input").val().length > 1 && firsttime) {
+            firsttime = false;
+            $(".ui.main.text.container").html($("#results-container").html());
+        } else if ($("#search-input").val() != "" && !firsttime) {
+            $(".ui.main.text.container").html($("#results-container").html());
+        } else {
+            $(".ui.main.text.container").html(globalhtml);
+        }
+    });
 
-$("#verzenden").click(function() {
-	$.get( "https://luithollander.nl/ozhv.php?naam=" + encodeURIComponent($("#naam").val()) + "&email=" + encodeURIComponent($("#email").val()) + "&bericht=" + encodeURIComponent($("#bericht").val()) + "");
-	$("form").fadeOut(500);
-	$(".verzonden").fadeIn(500);
-});
-$("form").submit(function(e){
+    $("#verzenden").click(function() {
+        $.get("https://luithollander.nl/ozhv.php?naam=" + encodeURIComponent($("#naam").val()) + "&email=" + encodeURIComponent($("#email").val()) + "&bericht=" + encodeURIComponent($("#bericht").val()) + "");
+        $("form").fadeOut(500);
+        $(".verzonden").fadeIn(500);
+    });
+    $("form").submit(function(e) {
         e.preventDefault();
     });
+    if ($(".ui.main.text.container.post").length) {
+        if (document.referrer.includes('archief')) {
+            $(".terug").attr("href", document.referrer);
+        } else if (!document.referrer.includes('{{ site.url }}')) {
+            $(".terug").html('<i class="arrow left icon"></i>Alle artikelen');
+        }
+    }
+    if ($(".ui.main.text.container.home").length) {
+        $(".ui.icon.input").show();
+    }
 });
